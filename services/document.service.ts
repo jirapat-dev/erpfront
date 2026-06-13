265045655a7cb5e55815fa581a7621466c77cf24
Jirapat Sriboonrueng <jirapat.sribo@gmail.com>
2026-06-13 23:21:59 +0700

    stashing document service

---
+++ services/document.service.ts
@@ -0,0 +1,29 @@
+import { api } from "@/lib/axios";
+import {
+    User,
+} from "@/types/user";
+
+export const DocumentService = {
+    getDocuments: async (userId: string): Promise<User> => {
+        const response = await api.get(`/users/${userId}`);
+        return response.data;
+    },
+
+    // createUser: async (
+    //     payload: CreateUserRequest
+    // ): Promise<User> => {
+    //     const response = await api.post("/users", payload);
+    //     return response.data;
+    // },
+
+    // updateUser: async (
+    //     payload: UpdateUserRequest
+    // ): Promise<User> => {
+    //     const response = await api.post(
+    //         `/users/${payload.id}`,
+    //         payload
+    //     );
+
+    //     return response.data;
+    // },
+};
